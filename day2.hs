a159fe8aa0391ddbeb0ac64bbd057744567b88a7
E. G. Bland <egb528@york.ac.uk>
2019-12-07 19:10:15 +0000

    add type to day2_2

--- day2.hs
+++ day2.hs
@@ -19,7 +19,7 @@ day2_1 = do
 
 day2_2 = do
     input <- readFile "day2.txt"
-    let ins = [read a :: Int | a <- splitAtCommas input]
+    let ins = [read a :: Opcode | a <- splitAtCommas input]
     let programs = [(zip (take 1 ins ++ [noun,verb] ++ drop 3 ins) [0..length ins-1],(noun,verb)) | noun<-[0..99], verb<-[0..99]]
     let resolvedPrograms = [(handleProgram program 0,nv) | (program,nv) <- programs]
     putStrLn ("Problem 2: " ++ (show $ let (_,nv) = head $ filter (\(prog,_) -> (readProgram prog 0 == 19690720)) resolvedPrograms in (100*fst nv) + snd nv))
