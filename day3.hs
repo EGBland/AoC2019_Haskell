bd504da4bb9641985c05e51fb0d81c340b734010
E. G. Bland <egb528@york.ac.uk>
2019-12-08 20:09:45 +0000

    add second problem of day 3

--- day3.hs
+++ day3.hs
@@ -7,32 +7,43 @@ data Orientation = Horizontal | Vertical deriving (Eq)
 
 main = do
     day3_1
-    --day3_2
+    day3_2
 
 day3_1 = do
     input <- readFile "input/day3.txt"
-    putStrLn ("Day 03, Problem 1: " ++ (show $ minimum $ map manhattan (getIntersects (sortLines $ processInstructions $ head $ lines input) (sortLines $ processInstructions $ last $ lines input))))
+    putStrLn ("Day 03, Problem 1: " ++ (show $ minimum $ map manhattan (getIntersects (processInstructions $ head $ lines input) (processInstructions $ last $ lines input))))
 
 day3_2 = do
     input <- readFile "input/day3.txt"
-    putStrLn ("Day 03, Problem 2: " ++ (show $ stepsToPoint (sortLines $ processInstructions "R8,U5,L5,D3") (3,3)))
+    let w1 = processInstructions $ head $ lines input
+    let w2 = processInstructions $ last $ lines input
+    putStrLn ("Day 03, Problem 2: " ++ (show $ minimum $ map (\p -> ((stepsToPoint w1 p) + (stepsToPoint w2 p))) (getIntersects w1 w2)))
     
-
 manhattan :: Point -> Int
 manhattan p = (abs $ fst p) + (abs $ snd p)
 
 stepsToPoint :: [Line] -> Point -> Int
-stepsToPoint (l:ls) p = if pointOnLine l p then lineLength l else (lineLength l) + (stepsToPoint ls p)
+stepsToPoint [] _ = error "point not on path"
+stepsToPoint (l:ls) p = if pointOnLine l p then lineToPoint l p else (lineLength l) + (stepsToPoint ls p)
 
-
+lineToPoint :: Line -> Point -> Int
+lineToPoint l (x,y)
+    | getOrient l == Horizontal = abs (x - x1)
+    | getOrient l == Vertical = abs (y - y1)
+    where x1 = fst $ fst l
+          y1 = snd $ fst l
 
 lineLength :: Line -> Int
-lineLength ((xl,yb),(xr,yt)) = if getOrient ((xl,yb),(xr,yt)) == Vertical then yt-yb else xr-xl
+lineLength ((xl,yb),(xr,yt)) = if getOrient ((xl,yb),(xr,yt)) == Vertical then abs (yt-yb) else abs (xr-xl)
 
 pointOnLine :: Line -> Point -> Bool
-pointOnLine ((xl,yb),(xr,yt)) (x,y)
+pointOnLine l (x,y)
     | getOrient ((xl,yb),(xr,yt)) == Vertical = xl == x && yb <= y && y <= yt
     | otherwise = yb == y && xl <= x && x <= xr
+    where xl = min (fst $ fst l) (fst $ snd l)
+          xr = max (fst $ fst l) (fst $ snd l)
+          yb = min (snd $ fst l) (snd $ snd l)
+          yt = max (snd $ fst l) (snd $ snd l)
 
 sortLines :: [Line] -> [Line]
 sortLines [] = []
@@ -49,14 +60,14 @@ getIntersects' (a:as) b
     | getOrient a == getOrient b = getIntersects' as b
     | getOrient b == Vertical   = if axl <= bxl && bxl <= axr && byb <= ayb && ayb <= byt then (bxl,ayb) : getIntersects' as b else getIntersects' as b
     | getOrient b == Horizontal = if ayb <= byb && byb <= ayt && bxl <= axl && axl <= bxr then (axl,byb) : getIntersects' as b else getIntersects' as b
-    where axl = fst $ fst $ a
-          axr = fst $ snd $ a
-          ayb = snd $ fst $ a
-          ayt = snd $ snd $ a
-          bxl = fst $ fst $ b
-          bxr = fst $ snd $ b
-          byb = snd $ fst $ b
-          byt = snd $ snd $ b
+    where axl = min (fst $ fst $ a) (fst $ snd $ a)
+          axr = max (fst $ fst $ a) (fst $ snd $ a)
+          ayb = min (snd $ fst $ a) (snd $ snd $ a)
+          ayt = max (snd $ fst $ a) (snd $ snd $ a)
+          bxl = min (fst $ fst $ b) (fst $ snd $ b)
+          bxr = max (fst $ fst $ b) (fst $ snd $ b)
+          byb = min (snd $ fst $ b) (snd $ snd $ b)
+          byt = max (snd $ fst $ b) (snd $ snd $ b)
     
 
 getOrient :: Line -> Orientation
